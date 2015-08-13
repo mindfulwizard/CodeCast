13d66d58bad5e969105bd4be3099f41a1cd42919
Kelsey <kelseysroberts@gmail.com>
2015-08-13 17:05:59 -0400

    merging branch

--- browser/js/modes/liveCast/liveDirective.js
+++ browser/js/modes/liveCast/liveDirective.js
@@ -3,12 +3,6 @@ app.directive('ng-interval', ['$interval', function($interval){
 		restrict: 'A',
 		require: 'ngModel',
 		controller: 'liveCtrl',
-		link: function(scope, element, attribute) {
-					$interval(function(){
-						scope.getText(textSnip);
-					}, 500)
-
-					}
 		}
 	
 }])
