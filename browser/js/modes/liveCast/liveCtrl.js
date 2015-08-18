63ac881a68605e626b4e471c4c36fc0a6cd5fde0
Nirmohi <nirmohi0605@gmail.com>
2015-08-18 18:28:01 -0400

    everything works together

--- browser/js/modes/liveCast/liveCtrl.js
+++ browser/js/modes/liveCast/liveCtrl.js
@@ -1,55 +1,56 @@
 app.controller('liveCtrl', function($scope, $interval, castFactory, $q, $document, $rootScope, socketFactory, $stateParams, evaluatorFactory) {
+
+  socketFactory.emit('join', $stateParams.roomId)
+  socketFactory.on('get code history', function(history) {
+    // if (history === $stateParams.roomId)
+    //console.log('history', history)
     $scope.replayObj = {
-        text: null
+      text: null
     };
-
-    socketFactory.emit('join', $stateParams.roomId)
-    socketFactory.on('get code history', function(history) {
-        // if (history === $stateParams.roomId)
-        //console.log('history', history)
-        $scope.replayObj.text = history;
-        //console.log('$scope', $scope)
-    })
+    $scope.replayObj.text = history;
+    //console.log('$scope', $scope)
+  })
 
 
-    // //listener for when codehistory changes on joining a room
-    // //everytime the instruction types, change the textsnip
-    socketFactory.on('change the textSnip', function(str) {
-        //console.log('str', str)
-        $scope.replayObj.text = str;
-    })
+  // //listener for when codehistory changes on joining a room
+  // //everytime the instruction types, change the textsnip
+  socketFactory.on('change the textSnip', function(str) {
+    //console.log('str', str)
+    $scope.replayObj.text = str;
+  })
 
-    var keystroke = false;
-    var timerPromise;
-    $scope.replayId;
-    $scope.evals = evaluatorFactory.liveEvals;
-
-    $scope.startRecording = function() {
-        if (!keystroke) {
-            keystroke = true;
-
-            castFactory.createReplay()
-                .then(function(replayId) {
-                    $scope.replayId = replayId;
-                    timerPromise = $interval(function() {
-                        castFactory.sendText($scope.replayObj.text, new Date(), replayId);
-                    }, 500);
-                })
-        }
-    }
+  var keystroke = false;
+  var timerPromise;
+  $scope.replayId;
+  $scope.evals = evaluatorFactory.liveEvals;
 
 
-    $scope.startSharing = function() {
-        // console.log('start sharing')
-        socketFactory.emit('instructor writing', {
-            data: $scope.replayObj.text,
-            roomId: $stateParams.roomId
-        })
-    }
+  $scope.startRecording = function() {
+    // console.log('startRecording')
 
-    $scope.endInterval = function() {
-        $interval.cancel(timerPromise);
-    }
+    // if (!keystroke) {
+    //   keystroke = true;
+    castFactory.createReplay()
+      .then(function(replayId) {
+        castFactory.sendText($scope.replayObj.text, new Date(), replayId, $stateParams.roomId);
+      })
+
+    // }
+
+  }
+
+
+  // $scope.startSharing = function() {
+  //   // console.log('start sharing')
+  //   socketFactory.emit('instructor writing', {
+  //     data: $scope.replayObj.text,
+  //     roomId: $stateParams.roomId
+  //   })
+  // }
+
+  $scope.endInterval = function() {
+    $interval.cancel(timerPromise);
+  }
 
 
 });
