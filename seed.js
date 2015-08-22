a0891deb194ca44b7427102737f29a2f7d38fd33
Xavier Durand-Smet <xavier.durand-smet>
2015-08-22 17:43:54 -0400

    auth required for room creation, teacher dashboard gets all his rooms&lectures

--- seed.js
+++ seed.js
@@ -34,7 +34,8 @@ var seedUsers = function () {
         },
         {
             email: 'obama@gmail.com',
-            password: 'potus'
+            password: 'potus',
+            instructor: false
         }
     ];
 
--- server/app/routes/roomRoutes.js
+++ server/app/routes/roomRoutes.js
@@ -2,17 +2,7 @@ var router = require('express').Router();
 var mongoose = require('mongoose');
 var Room = mongoose.model('Room');
 var User = mongoose.model('User');
-
-router.post('/', function(req, res) {
-	Room.create({
-		name: req.body.name,
-		instructor: req.body.instructor
-	})
-		.then(function(room) {
-			console.log('room created?', room)
-			res.send(room);
-		});
-});
+var Auth = require('./../configure/auth.middleware.js')
 
 router.get('/', function(req, res) {
 	Room.find({lectureEnded: false}).exec()
@@ -35,23 +25,44 @@ router.get('/:id', function(req, res) {
 		});
 });
 
-
 // find all the rooms by the instructor who created them
 router.get('/instructor/:instructorId', function(req, res) {
-	console.log('in get routes for one instructor')
-	Room.find({instructor: req.params.instructorId}).exec()
+	Room.find({instructor: req.params.instructorId, lectureStarted: false}).exec()
 		.then(function(room) {
 			res.json(room);
 		});
 });
 
+router.get('/lectures/:instructorId', function(req, res) {
+	Room.find({instructor: req.params.instructorId, lectureEnded: true}).exec()
+		.then(function(rooms) {
+			res.json(rooms);
+		});
+});
 
 router.put('/:id', function (req, res) {
 	Room.findByIdAndUpdate(req.params.id, req.body).exec()
 	.then(function (room) {
 		res.json(room);
 	});
-
 })
+
+//Auth authentication here
+router.use('/', Auth.isAuthenticated, function(req, res, next) {
+	if (req.user.instructor) next();
+	else Auth.isAdmin(req, res, next);
+});
+
+router.post('/', function(req, res) {
+	console.log('passed auth')
+	Room.create({
+		name: req.body.name,
+		instructor: req.body.instructor
+	})
+		.then(function(room) {
+			console.log('room created?', room)
+			res.send(room);
+		});
+});
 
 module.exports = router;
