5f844014ca5fe0c970dbfb9f1a577c16fa2666be
Isaac Ratner <isaacratner@gmail.com>
2015-08-23 02:04:45 -0400

    fixed if/else logic and added comments

--- browser/js/modes/replayCast/replayCtrl.js
+++ browser/js/modes/replayCast/replayCtrl.js
@@ -1,41 +1,53 @@
 app.controller('replayCtrl', function($scope, castFactory, $stateParams, $timeout) {
+    $scope.videoObj = {text: null, result: null};
     var sortedSlicesArr;
     var replayCurrentIndex;
-    var paused = false;
     var videoStarted = false;
-    $scope.videoObj = {text: null, result: null};
+    var videoOver = false;
+    var paused = false;
 
+    //custom sort to order codeSlices in array by timestamp
     var sortSlices = function(sliceList) {
         return sliceList.sort(function(a, b) {
             if (a.time < b.time) return -1;
             if (a.time > b.time) return 1;
             return 0;
-        })
+        });
     }
 
+    //add runningTotal property to each codeSlice = milliseconds relative to first slice in array
     var calculateRunningTotal = function(first, slice) {
         return slice.runningTotal = Date.parse(slice.time) - Date.parse(first.time);
     }
 
     var renderFullCast = function(sortedSlicesArr, currentIndex) {
         if(!paused){
+
+            //index to start/restart playback
             replayCurrentIndex = currentIndex;
             //console.log('currentSlicearr', sortedSlicesArr);
             var currentSlice = sortedSlicesArr[replayCurrentIndex];
+
+            //send currentSlice info to Angular to instantiate
             $scope.videoObj.text = currentSlice.text;
             $scope.videoObj.result = currentSlice.result;
             $scope.currentTime = currentSlice.runningTotal;
-            var next = sortedSlicesArr[replayCurrentIndex+1];
-                  
+
+            var next = sortedSlicesArr[replayCurrentIndex+1];      
             if(next){
+                //call renderFullCast recursively inside a timeout, passing in runningTotal diff as delay
                 $timeout(function(){
                     renderFullCast(sortedSlicesArr, replayCurrentIndex+1)
-                    console.log(replayCurrentIndex)
+                    //console.log(replayCurrentIndex)
                 }, next.runningTotal - currentSlice.runningTotal)
-            } 
+            }
+            else{
+                videoOver = true;
+            }
         }
-    };
+    }
 
+    //if user moves slider to a specific time, find the index of that slice to rerender the array  from there
     var userUpdatingTime = function() {
         sortedSlicesArr.forEach(function(slice, index) {
             if($scope.currentTime >= slice.runningTotal) {
@@ -55,10 +67,11 @@ app.controller('replayCtrl', function($scope, castFactory, $stateParams, $timeou
                 return sortSlices(sliceList);
             })
             .then(function(sortedArr) {
-                sortedSlicesArr = sortedArr
+                sortedSlicesArr = sortedArr;
                 sortedSlicesArr.forEach(function(slice) {
-                    calculateRunningTotal(sortedSlicesArr[0], slice)
-                })
+                    calculateRunningTotal(sortedSlicesArr[0], slice);
+                });
+
                 $scope.replayLength = _.last(sortedSlicesArr).runningTotal;
                 if(!videoStarted){
                     //console.log('starting')
@@ -75,17 +88,19 @@ app.controller('replayCtrl', function($scope, castFactory, $stateParams, $timeou
     $scope.pauseContinue = function() {
         if(!videoStarted){
             $scope.getFullCast();
-        }
-        else if(videoStarted && paused) {
+        } else if(paused) {
             paused = false;
             renderFullCast(sortedSlicesArr, replayCurrentIndex);
-
-        } else if(videoStarted && !paused) {
-            $scope.pauser();
-        }
+        } else if(!paused) {
+            paused = true;
+        } else if(videoOver){
+            videoOver = false;
+            renderFullCast(sortedSlicesArr, 0);
+        } 
     }
 
-    $scope.userUpdatingTime = _.debounce(userUpdatingTime, 300);
+    //since ng-change event continuously happens as long as user moves slider, debounce the function dependent on it
+    $scope.userUpdatingTime = _.debounce(userUpdatingTime, 250);
 
 });
 
