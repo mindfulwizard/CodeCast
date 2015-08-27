e9f46ce8b1d6593f8e29628a7955497fa2c703e8
Kelsey <kelseysroberts@gmail.com>
2015-08-27 15:25:22 -0400

    finished forks in student dash

--- browser/js/dashboards/students/studentDashboardCtrl.js
+++ browser/js/dashboards/students/studentDashboardCtrl.js
@@ -1,4 +1,4 @@
-app.controller('studentDashboardCtrl', function($scope, userFactory, castFactory, $stateParams) {
+app.controller('studentDashboardCtrl', function($scope, userFactory, forkFactory, castFactory, $stateParams) {
     $scope.instructors;
     $scope.instructorRooms;
     $scope.showRooms = false;
@@ -8,7 +8,9 @@ app.controller('studentDashboardCtrl', function($scope, userFactory, castFactory
     $scope.user;
     $scope.forkId = $stateParams.forkId;
     $scope.fork = "fork";
-    $scope.forkedLectures=[];
+    $scope.forkedLectures = [];
+    $scope.showForks = false;
+    $scope.roomForks = [];
 
     userFactory.getInstructors()
         .then(function(instructors) {
@@ -54,33 +56,49 @@ app.controller('studentDashboardCtrl', function($scope, userFactory, castFactory
 
     $scope.getCurrentUser();
 
-    $scope.getForkRoomList = function(){
-    userFactory.getForks()
-    .then(function(forks) {
-        $scope.forks = forks;
-        return forks
-    })
-    .then(function(forks){
-        var allRooms = $scope.liveLectures.concat($scope.replayLectures)
-        forks.forEach(function(fork){
-           allRooms.forEach(function(room){
-            if(room._id === fork.roomId) {
-                $scope.forkedLectures.push(room.name)
-            }
-           })
-        })
-        console.log($scope.forkedLectures)
-    })
-}
+    $scope.getForkRoomList = function() {
+        userFactory.getForks()
+            .then(function(forks) {
+                $scope.forks = forks;
+                return forks
+            })
+            .then(function(forks) {
+                var allRooms = $scope.liveLectures.concat($scope.replayLectures)
+                forks.forEach(function(fork) {
+                    allRooms.forEach(function(room) {
+                        if (room._id === fork.roomId) {
+                            if ($scope.forkedLectures.indexOf(room) === -1)
+                                $scope.forkedLectures.push(room)
+                        }
+                    })
+                })
+            })
+    }
+
+    $scope.getForkRoomList();
 
 
-    $scope.getFork = function(forkId) {
+    $scope.getRoomForks = function(roomId) {
+        if (!$scope.showForks) {
+            forkFactory.getUserForks(roomId)
+                .then(function(forks) {
+                    $scope.roomForks = forks;
+                    $scope.showForks = true;
+                })
+        } else {
+            $scope.showForks = false;
+        }
+    }
+
+    $scope.getFork = function() {
         $scope.forks.forEach(function(fork) {
-            if (fork._id === forkId) {
+            if (fork._id === $stateParams.forkId) {
                 $scope.forkedText = fork;
             }
         })
     }
+
+    if ($stateParams.forkId !== undefined) {$scope.getFork()};
 
 
     // $scope.credentials = {};
